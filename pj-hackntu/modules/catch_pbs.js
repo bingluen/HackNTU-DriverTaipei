023aa24b62ec339c8c244fc7cd0c66ca09fb5830
Erickson Juang <a820224@gmail.com>
2015-08-20 01:39:28 +0800

    add pbs api

---
+++ pj-hackntu/modules/catch_pbs.js
@@ -0,0 +1,38 @@
+var request = require('request');
+var moment = require('moment');
+
+var PBS = function() {
+  this.url = 'http://210.69.35.216/data/api/pbs';
+}
+
+PBS.prototype.catching = function(next) {
+
+  var filter = function(data) {
+    data = data.filter(function(element) {
+      return (element.region == 'N' && parseInt(element.x1) > 0 && parseInt(element.y1) > 0)
+    });
+
+    data = data.map(function(currentValue) {
+      var obj = {}
+      obj.id = currentValue.UID;
+      obj.status = '['+ currentValue.roadtype +']' + (currentValue.direction ? (currentValue.direction + '-') : '') + currentValue.comment;
+      obj.lat = currentValue.y1;
+      obj.lng = currentValue.x1;
+      var dt = moment(currentValue.happendate+'T'+currentValue.happentime);
+      obj.dt = dt.format('MM/DD hh:mm:ss')
+      return obj;
+    });
+
+    next(null, data)
+  }
+
+  request(this.url, function(err, res, body) {
+    if (!err && res.statusCode == 200) {
+      filter(JSON.parse(body).result);
+    } else {
+      next(err);
+    }
+  })
+}
+
+module.exports = PBS;
--- pj-hackntu/router/api.js
+++ pj-hackntu/router/api.js
@@ -6,6 +6,7 @@ var ErrorHandle = require('../modules/error_handle');
 var Parking = require('../modules/catch_parking');
 var Gas = require('../modules/catch_gas');
 var Construct = require('../modules/catch_construct');
+var Pbs = require('../modules/catch_pbs');
 
 //parking sapce
 router.get('/parking', function(req, res, next) {
@@ -46,6 +47,18 @@ router.get('/construct', function(req, res, next) {
       res.status(200).json(data);
     }
   });
+})
+
+router.get('/pbs', function(req, res, next) {
+  var pbsData = new Pbs();
+  pbsData.catch(function(err, data) {
+    if(err) {
+      var errorhandle = new ErrorHandle(err);
+      errorhandle.do();
+    } else {
+      res.status(200).json(data);
+    }
+  })
 })
 
 module.exports = router;
