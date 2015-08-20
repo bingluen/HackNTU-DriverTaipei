2b412c1b1fb33b706ec6fe6532a89806899a9dba
Erickson Juang <a820224@gmail.com>
2015-08-20 19:35:16 +0800

    implement error handle

--- pj-hackntu/modules/error_handle.js
+++ pj-hackntu/modules/error_handle.js
@@ -1,10 +1,20 @@
 var Errorhandle = function (err, type) {
   this.err = err;
+  this.statusCode = this.err.statusCode || 500
   this.type = type || 'html';
 }
 
-Errorhandle.prototype.do = function(err) {
-  
+Errorhandle.prototype.do = function(req ,res, next) {
+  if(this.type == 'html') {
+    res.status(this.statusCode).json({
+      err: 'err'
+    })
+  } else if(this.type == 'json') {
+    res.status(this.statusCode).json({
+      err: this.err.message,
+      statusCode: this.statusCode
+    });
+  }
 }
 
 module.exports = Errorhandle;
