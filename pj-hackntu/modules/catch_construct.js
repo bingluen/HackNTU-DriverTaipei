00bf9fc55dbb20b656f93eb0345ef9cb06c1916a
Erickson Juang <a820224@gmail.com>
2015-08-21 18:45:38 +0800

    add log and remove date

--- pj-hackntu/modules/catch_construct.js
+++ pj-hackntu/modules/catch_construct.js
@@ -1,18 +1,37 @@
 var request = require('request');
 var CoordinateTransforms = require('../helper/CoordinateTransform');
 
+var fs = require('fs-extra');
+var path = require('path');
+var moment = require('moment');
+
 var construct = function() {
   this.url = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=201d8ae8-dffc-4d17-ae1f-e58d8a95b162"
+  this.logfile = fs.createOutputStream(path.join(__dirname, '../log', moment().format('YYYY-MM-DD-h-mm-ss-') + 'log_construct.log'));
 }
+
+construct.prototype.writeLogfile = function (messages, type) {
+
+  this.logfile.write('[' + Date(Date.now()).toString() + ']' + (type ? '['+ type +']' : '') + messages + '\n');
+};
 
 
 construct.prototype.catching = function (next) {
   var parseDate = function(string) {
-    return string.slice(3, 5) + '/' + string.slice(5, 7);
+    //var d = new Date(string.slice(3, 5) + '/' + string.slice(5, 7));
+
+    var d = new Date((parseInt(string.slice(0, 3)) + 1911) + '/' + string.slice(3, 5) + '/' + string.slice(5, 7));
+
+    return d;
   }
 
 
   var filter = function(data) {
+    this.writeLogfile('catched construct data');
+    data = data.filter(function(element) {
+      return (parseDate(element.CE_DA) > Date.now() && element.NPURP != '其他')
+    })
+
     data = data.map(function(currentValue) {
       var obj = {}
       var latlng = CoordinateTransforms.TW97_WGS84(currentValue.X, currentValue.Y);
@@ -20,14 +39,13 @@ construct.prototype.catching = function (next) {
       obj.status = currentValue.NPURP;
       obj.lat = latlng.y;
       obj.lng = latlng.x;
-      obj.startDay = parseDate(currentValue.CB_DA);
-      obj.completeDay = parseDate(currentValue.CE_DA);
       return obj
     });
-
+    this.writeLogfile('send construct data');
     next(null, data);
-  }
+  }.bind(this)
 
+  this.writeLogfile('start to catching construct data');
   request(this.url, function(err, res, body) {
     if(!err) {
       filter(JSON.parse(body).result.results);
