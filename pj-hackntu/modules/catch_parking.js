10b5380c343c4905e77dc779123fb0b225f1719a
Erickson Juang <a820224@gmail.com>
2015-08-21 16:02:42 +0800

    renew parking data resource and add log

--- pj-hackntu/modules/catch_parking.js
+++ pj-hackntu/modules/catch_parking.js
@@ -1,59 +1,57 @@
 var request = require('request');
 var async = require('async');
 var CoordinateTransforms = require('../helper/CoordinateTransform');
+var fs = require('fs-extra');
+var path = require('path');
+
+var logfileName = 'log_parking.log';
 
 var Parking = function() {
   this.url = {
-    parking: "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=fe6767ec-9732-4bc9-b051-0990087c708d",
+    //parking: "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=fe6767ec-9732-4bc9-b051-0990087c708d",
+    parking: "http://data.taipei.gov.tw/opendata/apply/query/QzhBMEJFOTctMEEzRC00M0Q2LThDNDktNDVCNDc3NDNDRDBC?$format=json",
     available: "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=9ba187c9-b07e-40bc-9aa5-8d3c9f1aad63"
   };
 }
+
+Parking.prototype.writeLogfile = function (messages, type) {
+  fs.outputFile(path.join(__dirname, '../log', logfileName),
+    '[' + Date(Date.now()).toString() + ']' + (type ? '['+ type +']' : '') + messages,
+    function(err) {
+      if(err) {
+          console.log(err); //null
+      }
+  });
+};
 
 
 Parking.prototype.catching = function(next) {
 
   async.parallel({
     parkingData: function(callback) {
+      this.writeLogfile('start to catching parking data');
       request(this.url.parking, function(err, res, body) {
+        this.writeLogfile('catched parking data');
         if (body.length <= 0) {
 
           var err = {
             message: 'Cant\'t catch data.',
             statusCode: 500
           }
-
+          this.writeLogfile('Cant\'t catch parking data.', 'error');
           callback(err)
 
         } else if(!err && res.statusCode == 200) {
-          callback(null, JSON.parse(body).result.results);
+          callback(null, JSON.parse(body));
         } else {
           callback(err)
         }
-      })
-    }.bind(this),
-
-
-    availableSpace: function(callback) {
-      request(this.url.available, function(err, res, body) {
-        if (body.length <= 0) {
-
-          var err = {
-            message: 'Cant\'t catch data.',
-            statusCode: 500
-          }
-
-          callback(err);
-
-        } else if(!err && res.statusCode == 200) {
-          callback(null, JSON.parse(body).result.results);
-        } else {
-          callback(err)
-        }
-      })
+      }.bind(this))
     }.bind(this)
   },
 
   function(err, results) {
+    console.log(results);
     if(!err) {
       // 刪除無用的欄位
       var parkingData = results['parkingData'].map(function(currentValue) {
@@ -63,29 +61,12 @@ Parking.prototype.catching = function(next) {
         obj.name = currentValue.name;
         obj.lat = latlng.y;
         obj.lng = latlng.x;
+        obj.availableCar = (parseInt(currentValue.availableCar) > 0 ? parseInt(currentValue.availableCar) : 0);
+        obj.availableMotor = (parseInt(currentValue.availableMotor) > 0 ? parseInt(currentValue.availableMotor) : 0);
         obj.payDes = currentValue.payex;
         return obj;
       });
 
-      // before merge refactor available parking space array to object
-      var availableParking = {}
-      for(var i = 0 ; i < results['availableSpace'].length; i ++) {
-        availableParking[results['availableSpace'][i].id] = {
-          availableCar: (results['availableSpace'][i].availableCar > 0 ? results['availableSpace'][i].availableCar : 0),
-          availableMotor: (results['availableSpace'][i].availableMotor > 0 ? results['availableSpace'][i].availableMotor : 0)
-        }
-      }
-
-      // merge available parking space by loop
-      for(var i = 0; i < parkingData.length; i ++) {
-        if(availableParking[parkingData[i].id]) {
-          parkingData[i].availableCar = availableParking[parkingData[i].id].availableCar || 0;
-          parkingData[i].availableMotor = availableParking[parkingData[i].id].availableMotor || 0;
-        } else {
-          parkingData[i].availableCar = 0;
-          parkingData[i].availableMotor = 0;
-        }
-      }
 
       // filte data (delete data which availableParking = zero)
       parkingData = parkingData.filter(function(element) {
